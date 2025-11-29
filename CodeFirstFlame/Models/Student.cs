e05787d78e57ea33c355a5b522681471de701a9f
Fedor_ex16@student.itstep.org <Fedor_ex16@student.itstep.org>
2025-11-29 14:26:50 +0200

    #29.11

---
+++ CodeFirstFlame/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstFlame.Models
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDay { get; set; }
+        public decimal Scholarship { get; set; }
+
+        public override string ToString()
+        {
+            return $"StudentId: {StudentId}, Name: {Name}, Surname: {Surname}, BirthDay: {BirthDay}, Stepyha: {Scholarship}";
+        }
+
+
+    }
+}
---
+++ CodeFirstFlame/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeFirstFlame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentDBContext())
+            {
+                var students = context.Students.ToList();
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+        }
+    }
+}
